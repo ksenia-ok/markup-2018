84a7ce3747f3598061ade1d63317e1746e073a1b
Ksenia <ksenia-ok@inbox.ru>
2018-10-31 02:38:09 +0300

    Ghost Host

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#carousel').owlCarousel({
+        loop:true,
+        nav:false, 
+        smartSpeed:1000, 
+        autoplayTimeout:2000,
+        items:1
+    });
+});
